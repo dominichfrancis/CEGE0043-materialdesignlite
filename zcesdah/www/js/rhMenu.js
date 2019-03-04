b4a675b1f0e46830c3bfab644888c8447fb13813
Dominic Heyna-Francis <zcesdah@ucl.ac.ugit config --global user.email zcesdah@ucl.ac.u>
2019-03-04 15:24:05 -0500

    working index.html with links to rhmenu function for logo pop up, new

--- zcesdah/www/js/rhMenu.js
+++ zcesdah/www/js/rhMenu.js
@@ -1,8 +1,10 @@
 
+//Create function to show a pop up alert when a menu button is pushed//
 function menuClicked() {
 	alert("You clicked the menu");
 }
 
+//Create function to show ucllogo linked to the imagtes file in wwww//
 function addUCLLogo() {
-document.getElementById(“ucllogo”).innerHTML =”<img src=’ images/ucl.png’>”
+	document.getElementById("ucllogo").innerHTML ="<img src=' images/ucl.png'>"
 }
