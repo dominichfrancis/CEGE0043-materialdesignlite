2d13460c5d0db7d8432d226b055a9d3775f49455
Dominic Heyna-Francis <zcesdah@ucl.ac.ugit config --global user.email zcesdah@ucl.ac.u>
2019-04-02 15:02:36 -0400

    finally with AJAX codfe working request and send but need to change AJAX.js file so that portnumber is not hard coded

--- zcesdah/www/js/AJAX.js
+++ zcesdah/www/js/AJAX.js
@@ -2,11 +2,11 @@ var xhrNode;
 
 function callDivNodeJSChange() {
 	xhrNode = new XMLHttpRequest();
-	var url = "http://developer.cege.ucl.ac.uk:"+httpPortNumber;
+	var url = "http://developer.cege.ucl.ac.uk:"+ 30279;
 	xhrNode.open("GET", url, true);
 	xhrNode.onreadystatechange = processDivNodeJSChange;
 	try {
-		xhrNode.setRequestHeader("Content-Type", "application/x-www-formurlencoded");
+		xhrNode.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 	}
 	catch (e) {
 	// this only works in internet explorer
@@ -15,11 +15,12 @@ function callDivNodeJSChange() {
 }
 
 function processDivNodeJSChange() {
-if (xhrNode.readyState < 4) // while waiting response from server
-	document.getElementById('ajaxtext').innerHTML = "Loading...";
-	else if (xhrNode.readyState === 4) { // 4 = Response from server has been completely loaded.
-		if (xhrNode.status == 200 && xhrNode.status < 300)
+if (xhrNode.readyState < 4){ // while waiting response from server
+	document.getElementById('ajaxtext').innerHTML = "Loading...";}
+
+	else if (xhrNode.readyState === 4){ // 4 = Response from server has been completely loaded.
+		if (xhrNode.status == 200 && xhrNode.status < 300){
 		// http status between 200 to 299 are all successful
-	        document.getElementById('ajaxtext').innerHTML = xhrNode.responseText;
+		document.getElementById('ajaxtext').innerHTML = xhrNode.responseText;}
 	}
 }
