a9a84016823019ce68fd517dcdcd4f62e60c5299
Dominic Heyna-Francis <zcesdah@ucl.ac.ugit config --global user.email zcesdah@ucl.ac.u>
2019-04-02 09:37:07 -0400

    app with the test simple ajax process working delivering test.hml to div under map

--- zcesdah/platforms/browser/www/js/AJAX.js
+++ zcesdah/platforms/browser/www/js/AJAX.js
@@ -1,26 +1,25 @@
-var xhrNode; // define the global variable to process the AJAX request
+var xhrNode;
 
 function callDivNodeJSChange() {
 	xhrNode = new XMLHttpRequest();
-	var url = "http:developer.cege.ucl.ac.uk:"+httpPortNumber;
+	var url = "http://developer.cege.ucl.ac.uk:"+httpPortNumber;
 	xhrNode.open("GET", url, true);
 	xhrNode.onreadystatechange = processDivNodeJSChange;
 	try {
-		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		xhrNode.setRequestHeader("Content-Type", "application/x-www-formurlencoded");
 	}
 	catch (e) {
-		// this ONLY works in internet explorer!!!!!!
+	// this only works in internet explorer
 	}
-		xhrNode.send();
-
+	xhrNode.send();
 }
 
 function processDivNodeJSChange() {
-if (xhrNode.readyState < 4){ // while waiting response from server
-	document.getElementById('ajaxtext').innerHTML = "Loading...";}
-
-    else if (xhrNode.readyState === 4){ // 4 = Response from server has been completely loaded.
-   	     if (xhrNode.status == 200 && xhrNode.status < 300){ // http status between 200 to 299 are all successful
-   	       document.getElementById('ajaxtext').innerHTML = xhrNode.responseText;}
-    }
+if (xhrNode.readyState < 4) // while waiting response from server
+	document.getElementById('ajaxtext').innerHTML = "Loading...";
+	else if (xhrNode.readyState === 4) { // 4 = Response from server has been completely loaded.
+		if (xhrNode.status == 200 && xhrNode.status < 300)
+		// http status between 200 to 299 are all successful
+	        document.getElementById('ajaxtext').innerHTML = xhrNode.responseText;
+	}
 }
