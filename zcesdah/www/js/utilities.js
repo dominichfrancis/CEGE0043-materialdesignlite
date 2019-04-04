3d42da8ee177f540c2271050381f17aa663f4db7
Dominic Heyna-Francis <zcesdah@ucl.ac.ugit config --global user.email zcesdah@ucl.ac.u>
2019-04-04 13:55:18 -0400

    code with formdata from my entiries, entries being sstored not accessing points in scotland??

--- zcesdah/www/js/utilities.js
+++ zcesdah/www/js/utilities.js
@@ -1,21 +1,20 @@
-
 var httpPortNumber;
 var httpsPortNumber;
-function getPort() {
-	var xhrNode = new XMLHttpRequest();
-	xhrNode.addEventListener("load", function () {
-		var parser = new DOMParser();
-		var doc = parser.parseFromString(xhrNode.responseText, "application/xml");
-		httpPortNumber= doc.getElementsByTagName("node-port-http") .item(0).textContent;
-		httpsPortNumber= doc.getElementsByTagName("node-port-https") .item(0).textContent;
-		console.log("Port : " + httpPortNumber);
-	});
+function getPort(){
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", function () {
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(xhr.responseText, "application/xml");
+        httpPortNumber= doc.getElementsByTagName("node-port-http").item(0).textContent;
+        httpsPortNumber= doc.getElementsByTagName("node-port-https").item(0).textContent;
+        console.log("Port : " + httpPortNumber);
+    });
 
-	// depending on whether we are in a browser or on a phone
+    // depending on whether we are in a browser or on a phone
     // the location of the config file is different
     // if we are on a phone then http and https won't be present
     var configLocation = "res/port.xml";
-    xhrNode.open("get", configLocation, true);
-    xhrNode.send();
+    xhr.open("get", configLocation, true);
+    xhr.send();
 
 }
